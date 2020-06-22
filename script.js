dfe382864dc77ecc42e50ac1a1c7c6b8026ced4d
Zachary Deacon <zackdeacon347@gmail.com>
2020-06-22 12:39:59 -0700

    created queryURL

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 
 $("button").on("click", function() {
-    var searchTerm = //get search results here from search bar;
+    var searchTerm = "pizza";
     var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" +
-      searchTerm + "&api_key=9i2yCPGHzonOCXAjz9iGZXWOsUcWyXC2";
+      searchTerm + "&api-key=9i2yCPGHzonOCXAjz9iGZXWOsUcWyXC2";
 
 
             $.ajax({
                 url: queryURL,
                 method: "GET"
             }).then(function(response) {
-
+                console.log(response)
             })
 
 })
